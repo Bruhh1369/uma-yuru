f539b99d9504d22b1aa6002741f3c2d10c9802ce
Bruhh1369 <bruhh136969@gmail.com>
2025-10-26 13:48:08 +0700

    icon

--- app/tools/rank-counter/parts/rank-result/FinalRankIcon.tsx
+++ app/tools/rank-counter/parts/rank-result/FinalRankIcon.tsx
@@ -2,23 +2,23 @@ import Image from "next/image";
 
 const FinalRankIcon = ({ rating }: { rating: number }) => {
     const rankTable = [
-        { max: 599, src: "/ranks/gp.png" },
-        { max: 899, src: "/ranks/f.png" },
-        { max: 1299, src: "/ranks/fp.png" },
-        { max: 1799, src: "/ranks/e.png" },
-        { max: 2299, src: "/ranks/ep.png" },
-        { max: 2899, src: "/ranks/d.png" },
-        { max: 3499, src: "/ranks/dp.png" },
-        { max: 4899, src: "/ranks/c.png" },
-        { max: 6499, src: "/ranks/cp.png" },
-        { max: 8199, src: "/ranks/b.png" },
-        { max: 9999, src: "/ranks/bp.png" },
-        { max: 12099, src: "/ranks/a.png" },
-        { max: 14499, src: "/ranks/ap.png" },
-        { max: 15899, src: "/ranks/s.png" },
-        { max: 17499, src: "/ranks/sp.png" },
-        { max: 19199, src: "/ranks/ss.png" },
-        { max: 19599, src: "/ranks/ssp.png" }
+        { max: 899, src: "/ranks/final/f.png" },
+        { max: 599, src: "/ranks/final/gp.png" },
+        { max: 1299, src: "/ranks/final/fp.png" },
+        { max: 1799, src: "/ranks/final/e.png" },
+        { max: 2299, src: "/ranks/final/ep.png" },
+        { max: 2899, src: "/ranks/final/d.png" },
+        { max: 3499, src: "/ranks/final/dp.png" },
+        { max: 4899, src: "/ranks/final/c.png" },
+        { max: 6499, src: "/ranks/final/cp.png" },
+        { max: 8199, src: "/ranks/final/b.png" },
+        { max: 9999, src: "/ranks/final/bp.png" },
+        { max: 12099, src: "/ranks/final/a.png" },
+        { max: 14499, src: "/ranks/final/ap.png" },
+        { max: 15899, src: "/ranks/final/s.png" },
+        { max: 17499, src: "/ranks/final/sp.png" },
+        { max: 19199, src: "/ranks/final/ss.png" },
+        { max: 19599, src: "/ranks/final/ssp.png" }
     ];
     const rank = rankTable.find((r) => rating <= r.max);
 
